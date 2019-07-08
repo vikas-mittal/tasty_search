4c7f5489cb747707ab39ed63b9ec88d7d1853e5b
Vikas Mittal <94vikasmittal@gmail.com>
2019-07-08 21:22:52 +0530

    Added constants.py to store constant values

---
+++ app/easy_search/constants.py
@@ -0,0 +1,4 @@
+col_names = ["product/productId", "review/userId", "review/profileName", "review/helpfulness",
+             "review/score", "review/time", "review/summary", "review/text"]
+
+data_file_path = "'C:/Users/Vikas Mittal/Desktop/data/foods.txt'"
