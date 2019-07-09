07266c4a6f6ee5cd21d5a3908818b886d5104420
Vikas Mittal <94vikasmittal@gmail.com>
2019-07-09 14:26:23 +0530

    Added func restructure_data_and_insert_in_redis() for structuring data & inserting into redis

--- app/easy_search/helper.py
+++ app/easy_search/helper.py
@@ -15,3 +15,15 @@ def dump_to_json(object):
     :return: dumpped json object
     """
     return json.loads(json.dumps(object))
+
+
+def restructure_data_and_insert_in_redis():
+    """
+    It convert the data stored in file into data frame. Also create a dictionary for each word occurrence in the
+    data frame in review text and summary text. At the end insert those into redis for future use.
+    """
+    df = convert_data_in_df()
+    review_each_word_occ_dict = get_json_of_each_word_occurrence_for_each_product(df, const.review_text_col_name)
+    summary_each_word_occ_dict = get_json_of_each_word_occurrence_for_each_product(df, const.summary_text_col_name)
+    insert_json_data_to_redis(review_each_word_occ_dict, "review_text_json")
+    insert_json_data_to_redis(summary_each_word_occ_dict, "summary_text_json")
