989b65deb051b729a0780f8e1e74959929c0cd83
Vikas Mittal <94vikasmittal@gmail.com>
2019-07-10 02:57:10 +0530

    Commented Unnecessary port and host variable in config

--- config/development/__init__.py
+++ config/development/__init__.py
@@ -5,8 +5,8 @@ TESTING = True
 SECRET_KEY = 'my precious'
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
-HOST = 'localhost'
-PORT = int(os.environ.get('PORT', 5000))
+# HOST = 'localhost'
+# PORT = int(os.environ.get('PORT', 5000))
 
 PRODUCTION = False
 LOG_LEVEL = logging.INFO
