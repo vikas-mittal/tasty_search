b8b22419001b47a8a333ff59cf15c690d9c5c834
Vikas Mittal <94vikasmittal@gmail.com>
2019-07-10 00:15:12 +0530

    Add data file foods.txt path constant

--- app/easy_search/constants.py
+++ app/easy_search/constants.py
@@ -1,7 +1,9 @@
+import os
+
 col_names = ["product/productId", "review/userId", "review/profileName", "review/helpfulness",
              "review/score", "review/time", "review/summary", "review/text"]
 
-data_file_path = 'C:/Users/Vikas Mittal/Desktop/data/foods.txt'
+data_file_path = os.path.normpath(os.path.join(os.getcwd(), "foods.txt"))
 
 review_text_col_name = "review/text"
 summary_text_col_name = "review/summary"
