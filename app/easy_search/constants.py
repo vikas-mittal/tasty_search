95365efe7b68eff18b4e266b517a557d56007b97
Vikas Mittal <94vikasmittal@gmail.com>
2019-07-09 13:58:31 +0530

    Added review_text and summary_text const names

--- app/easy_search/constants.py
+++ app/easy_search/constants.py
@@ -1,4 +1,7 @@
 col_names = ["product/productId", "review/userId", "review/profileName", "review/helpfulness",
              "review/score", "review/time", "review/summary", "review/text"]
 
-data_file_path = "'C:/Users/Vikas Mittal/Desktop/data/foods.txt'"
+data_file_path = 'C:/Users/Vikas Mittal/Desktop/data/foods.txt'
+
+review_text_col_name = "review/text"
+summary_text_col_name = "review/summary"
